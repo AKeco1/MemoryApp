2f1668de1cec356bfa06d372e3930906e48577d3
arminkeco <armin.keco@hotmail.com>
2022-09-06 10:19:31 +0200

    Initial Commit

---
+++ IosTry/IosTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosTryApp.swift
+//  IosTry
+//
+//  Created by Armin Keco on 06.09.22.
+//
+
+import SwiftUI
+
+@main
+struct IosTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
